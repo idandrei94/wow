c18544d173575f54095db9cd47816e9434a3420a
iorga dan <idandrei94@gmail.com>
2021-02-12 21:18:32 +0100

    date

--- Account/3NTROPY/SavedVariables/Grid.lua
+++ Account/3NTROPY/SavedVariables/Grid.lua
@@ -1,17 +1,5 @@
 
 GridDB = {
-	["disabled"] = {
-		["Default"] = true,
-	},
-	["disabledModules"] = {
-		["Default"] = {
-			["GridFrame"] = true,
-			["GridRoster"] = true,
-			["GridRange"] = true,
-			["GridStatus"] = true,
-			["GridLayout"] = true,
-		},
-	},
 	["namespaces"] = {
 		["GridStatusRange"] = {
 			["profiles"] = {
@@ -120,19 +108,6 @@ GridDB = {
 						["range"] = false,
 						["desc"] = "More than 28 yards away",
 					},
-					["alert_range_60"] = {
-						["color"] = {
-							["a"] = 0.8363636363636364,
-							["b"] = 0.8,
-							["g"] = 0.2,
-							["r"] = 0.6,
-						},
-						["priority"] = 86,
-						["enable"] = true,
-						["text"] = "60 yards",
-						["range"] = false,
-						["desc"] = "More than 60 yards away",
-					},
 					["alert_range_30"] = {
 						["color"] = {
 							["a"] = 0.4545454545454546,
@@ -145,6 +120,19 @@ GridDB = {
 						["text"] = "30 yards",
 						["range"] = false,
 						["desc"] = "More than 30 yards away",
+					},
+					["alert_range_60"] = {
+						["color"] = {
+							["a"] = 0.8363636363636364,
+							["b"] = 0.8,
+							["g"] = 0.2,
+							["r"] = 0.6,
+						},
+						["priority"] = 86,
+						["enable"] = true,
+						["text"] = "60 yards",
+						["range"] = false,
+						["desc"] = "More than 60 yards away",
 					},
 					["alert_range_100"] = {
 						["color"] = {
