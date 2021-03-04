bf39928496a8e60918ca9dbcf2e6c3da1d9e3b91
iorga dan <idandrei94@gmail.com>
2021-03-04 21:37:48 +0100

    date

--- Account/3NTROPY/Icecrown/Kalaam/SavedVariables/Bagnon.lua
+++ Account/3NTROPY/Icecrown/Kalaam/SavedVariables/Bagnon.lua
@@ -2,15 +2,15 @@
 BagnonFrameSettings = {
 	["frames"] = {
 		["inventory"] = {
-			["point"] = "TOPRIGHT",
-			["itemFrameColumns"] = 12,
 			["y"] = -190.9139581159617,
 			["x"] = -173.5113774140609,
+			["point"] = "TOPRIGHT",
+			["itemFrameColumns"] = 12,
 		},
 		["bank"] = {
-			["point"] = "TOPLEFT",
 			["y"] = -111.1109122714131,
 			["x"] = 177.3035100229364,
+			["point"] = "TOPLEFT",
 		},
 	},
 	["version"] = "2.13.3",
--- Account/3NTROPY/Icecrown/Nalar/SavedVariables/Bagnon.lua
+++ Account/3NTROPY/Icecrown/Nalar/SavedVariables/Bagnon.lua
@@ -8,10 +8,10 @@ BagnonFrameSettings = {
 			["x"] = -195.9527211658713,
 		},
 		["bank"] = {
-			["point"] = "TOPLEFT",
-			["itemFrameColumns"] = 12,
 			["y"] = -130.316167475152,
 			["x"] = 382.380336316134,
+			["point"] = "TOPLEFT",
+			["itemFrameColumns"] = 12,
 		},
 	},
 	["version"] = "2.13.3",
