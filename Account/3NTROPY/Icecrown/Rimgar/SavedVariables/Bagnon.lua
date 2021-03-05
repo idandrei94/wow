128b21bc7ebf76f4815af2f7dde6f36bd3e44b14
iorga dan <idandrei94@gmail.com>
2021-03-05 09:16:49 +0100

    date

--- Account/3NTROPY/Icecrown/Rimgar/SavedVariables/Bagnon.lua
+++ Account/3NTROPY/Icecrown/Rimgar/SavedVariables/Bagnon.lua
@@ -7,9 +7,9 @@ BagnonFrameSettings = {
 			["x"] = -120.0043959317525,
 		},
 		["bank"] = {
+			["itemFrameColumns"] = 16,
 			["y"] = 123.2590332875572,
 			["x"] = 64.00031075161611,
-			["itemFrameColumns"] = 16,
 		},
 	},
 	["version"] = "2.13.3",
