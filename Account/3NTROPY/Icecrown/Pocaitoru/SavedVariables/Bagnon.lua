954621d8bb31105c99c848e86e1c34114a413caf
Dan Andrei Iorga <idandrei94@gmail.com>
2022-01-06 12:48:38 +0100

    date

--- Account/3NTROPY/Icecrown/Pocaitoru/SavedVariables/Bagnon.lua
+++ Account/3NTROPY/Icecrown/Pocaitoru/SavedVariables/Bagnon.lua
@@ -2,19 +2,19 @@
 BagnonFrameSettings = {
 	["frames"] = {
 		["inventory"] = {
-			["itemFrameColumns"] = 12,
 			["y"] = 93.6689755338496,
 			["x"] = -165.4907293916251,
+			["itemFrameColumns"] = 12,
 		},
 		["keys"] = {
 			["y"] = 149.9999976748339,
 			["x"] = -350.0000879459899,
 		},
 		["bank"] = {
-			["point"] = "TOPLEFT",
-			["itemFrameColumns"] = 15,
 			["y"] = -117.901509108052,
 			["x"] = 42.50789301582324,
+			["point"] = "TOPLEFT",
+			["itemFrameColumns"] = 15,
 		},
 	},
 	["version"] = "2.13.3",
