041caee3ec5a9a4b8bfd2fc3d1dce2658fe084e9
Dan Andrei Iorga <idandrei94@gmail.com>
2023-10-19 12:55:16 +0200

    date

--- Account/3NTROPY/SavedVariables/AutoDestroy.lua
+++ Account/3NTROPY/SavedVariables/AutoDestroy.lua
@@ -3,6 +3,6 @@ AutoDestroyCFG = {
 	["zone"] = "Elwynn Forest",
 	["minlevel"] = 20,
 	["weapons"] = 2,
+	["list"] = "Shiny Red Apple\nKobold Mining Shovel\nRefreshing Spring Water\nLinen Cloth\nMinor Healing Potion\nJourneyman's Pants\nRagged Leather Bracers\nWorn Leather Gloves\nRough Stone\nA Gold Tooth\nBurnt Leather Bracers\nFlimsy Chain Boots\nFine Scimitar\nRagged Leather Vest\nTough Jerky\nRagged Leather Pants\nRagged Leather Gloves\nLoose Chain Cloak\nFrayed Gloves\n",
 	["armor"] = 2,
-	["list"] = "Shiny Red Apple\nKobold Mining Shovel\nRefreshing Spring Water\nLinen Cloth\nMinor Healing Potion\nJourneyman's Pants\nRagged Leather Bracers\nWorn Leather Gloves\nRough Stone\nA Gold Tooth\nBurnt Leather Bracers\nFlimsy Chain Boots\nFine Scimitar\nRagged Leather Vest\nTough Jerky\n",
 }
