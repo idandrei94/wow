0d78752602f2df7377c45d82416a1f752ba18062
Dan Andrei Iorga <idandrei94@gmail.com>
2023-11-19 16:29:33 +0100

    date

--- Account/3NTROPY/Icecrown/Rimgar/SavedVariables/Bagnon.lua
+++ Account/3NTROPY/Icecrown/Rimgar/SavedVariables/Bagnon.lua
@@ -2,14 +2,14 @@
 BagnonFrameSettings = {
 	["frames"] = {
 		["inventory"] = {
-			["y"] = 127.0165718010279,
-			["x"] = -66.67052498353405,
 			["itemFrameColumns"] = 13,
+			["y"] = 121.0906522781739,
+			["x"] = -166.2260289903084,
 		},
 		["bank"] = {
+			["itemFrameColumns"] = 16,
 			["y"] = 194.3700588082388,
 			["x"] = 88.29665782670413,
-			["itemFrameColumns"] = 16,
 		},
 	},
 	["version"] = "2.13.3",
