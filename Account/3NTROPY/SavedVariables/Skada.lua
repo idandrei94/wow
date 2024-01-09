c75fb7bdc5c1a88f5d9c93437af031f4f492a4b5
Dan Andrei Iorga <idandrei94@gmail.com>
2024-01-09 17:12:46 +0100

    date

--- Account/3NTROPY/SavedVariables/Skada.lua
+++ Account/3NTROPY/SavedVariables/Skada.lua
@@ -4,16 +4,6 @@ SkadaDB = {
 		["LibDualSpec-1.0"] = {
 		},
 	},
-	["global"] = {
-		["version"] = 1878,
-		["nicknames"] = {
-			["reset"] = 1704562890,
-			["cache"] = {
-				["0x0700000000753EB5"] = "Oyo",
-				["0x0700000000933AFA"] = "Markan",
-			},
-		},
-	},
 	["profileKeys"] = {
 		["Rimgar - Icecrown"] = "Default",
 		["Praxia - Icecrown"] = "Default",
@@ -23,21 +13,29 @@ SkadaDB = {
 		["Eutanasie - Icecrown"] = "Default",
 		["Sculamare - Icecrown"] = "Default",
 		["Urgash - Icecrown"] = "Default",
-		["Vrael - Icecrown"] = "Default",
-		["Glacion - Icecrown"] = "Default",
 		["Pocaitoru - Icecrown"] = "Default",
 		["Glacianos - Icecrown"] = "Default",
+		["Lingurita - Icecrown"] = "Default",
+		["Elgorath - Icecrown"] = "Default",
 		["Pocaitorul - Icecrown"] = "Default",
 		["Flocparlit - Icecrown"] = "Default",
 		["Sculatru - Icecrown"] = "Default",
-		["Kalaam - Icecrown"] = "Default",
+		["Flokus - Icecrown"] = "Default",
 		["Maciucaru - Icecrown"] = "Default",
 		["Morridin - Icecrown"] = "Default",
-		["Flokus - Icecrown"] = "Default",
+		["Kalaam - Icecrown"] = "Default",
 		["Inarya - Icecrown"] = "Default",
 		["Pubianus - Icecrown"] = "Default",
-		["Elgorath - Icecrown"] = "Default",
-		["Lingurita - Icecrown"] = "Default",
+		["Glacion - Icecrown"] = "Default",
+		["Vrael - Icecrown"] = "Default",
+	},
+	["global"] = {
+		["nicknames"] = {
+			["cache"] = {
+			},
+			["reset"] = 1706105069,
+		},
+		["version"] = 1878,
 	},
 	["profiles"] = {
 		["Default"] = {
@@ -48,7 +46,7 @@ SkadaDB = {
 			},
 			["namedisplay"] = 2,
 			["modules"] = {
-				["deathlogthreshold"] = 1000,
+				["sunderchannel"] = "SAY",
 				["threat"] = {
 					["soundfile"] = "Fel Nova",
 					["ignorePets"] = true,
@@ -56,8 +54,8 @@ SkadaDB = {
 					["threshold"] = 90,
 					["notankwarnings"] = true,
 					["output"] = 1,
-					["showAggroBar"] = true,
 					["sound"] = true,
+					["showAggroBar"] = true,
 					["frequency"] = 2,
 				},
 				["failschannel"] = "AUTO",
@@ -65,12 +63,18 @@ SkadaDB = {
 				["deathchannel"] = "AUTO",
 				["interruptchannel"] = "SAY",
 				["parrychannel"] = "AUTO",
-				["sunderchannel"] = "SAY",
+				["deathlogthreshold"] = 1000,
 			},
 			["prepotion"] = true,
 			["windows"] = {
 				{
 					["barslocked"] = true,
+					["background"] = {
+						["color"] = {
+							["a"] = 0.9000000059604645,
+						},
+						["height"] = 290,
+					},
 					["hidden"] = false,
 					["y"] = 140.7405098856832,
 					["x"] = -644.7408003946764,
@@ -78,12 +82,6 @@ SkadaDB = {
 					["mode"] = "Damage",
 					["snapto"] = true,
 					["barwidth"] = 221.0367141682152,
-					["background"] = {
-						["height"] = 290,
-						["color"] = {
-							["a"] = 0.9000000059604645,
-						},
-					},
 				}, -- [1]
 			},
 			["report"] = {
